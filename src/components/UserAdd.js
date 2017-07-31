d192e8c6e132b4303bd9fe0d4c6fff1931063be6
ainibaoer2014 <756367019@qq.com>
2017-07-31 12:48:46 +0800

    在跳转时进行提示

--- src/components/UserAdd.js
+++ src/components/UserAdd.js
@@ -1,5 +1,16 @@
 import React,{Component} from 'react';
+import {Prompt} from 'react-router-dom';
 export default class UserAdd extends Component{
+    constructor(props){
+        super(props);
+        //默认不阻止跳转
+        this.state={blocking:false};
+    }
+    handleChange=(event)=>{
+        this.setState({
+            blocking:event.target.value && event.target.value.length>0
+        })
+    };
     handleSubmit=()=>{
          //获取输入框的值
         let name=this.name.value;
@@ -11,28 +22,39 @@ export default class UserAdd extends Component{
         users.push({id:Date.now(),name});
         //将数据 又 转换为字符串 存储在本地缓存中
         localStorage.setItem('users',JSON.stringify(users));
+        this.setState({
+            blocking:false
+        },()=>{
+            this.props.history.push('/user/list');
+        });
         //提交完数据后 跳转到 用户列表
         //用于跳转页面  this.props.history中用于跳转页面的方法：
         // go():参数：数字 作用：向前或后退几个页面
         // goBack()：无参数 作用：后退到上一页
         // goForward()：无参数  作用：向前到下一页
         // push()：参数：路径  作用：跳转到具体路径的页面
-        this.props.history.push('/user/list');
     };
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="name" className="control-label">姓名</label>
-                    {/*ref:引用真实的DOM元素
-                       ref=>this.name=ref : 当页面挂载真正的Input元素后，会执行该函数，将Input元素挂载在当前组件的name属性上
-                    */}
-                    <input type="text" className="form-control" ref={ref=>this.name=ref}/>
-                </div>
-                <div className="form-group">
-                    <input type="submit" className="btn btn-primary"/>
-                </div>
-            </form>
+            <div>
+                <Prompt
+                    when={this.state.blocking}
+                    message={(location)=>(`你确定要跳转到 ${location.pathname}吗？`)}
+                />
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name" className="control-label">姓名</label>
+                        {/*ref:引用真实的DOM元素
+                         ref=>this.name=ref : 当页面挂载真正的Input元素后，会执行该函数，将Input元素挂载在当前组件的name属性上
+                         */}
+                        <input type="text" className="form-control" ref={ref=>this.name=ref} onChange={this.handleChange}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" className="btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+
         )
     }
 }
